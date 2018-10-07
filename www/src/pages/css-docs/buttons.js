8751cd5fa56420486036e5247cd0e4d68c3d37d2
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-10-07 13:42:48 +0530

    add docs for cards and buttons

---
+++ www/src/pages/css-docs/buttons.js
@@ -0,0 +1,274 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+class ButtonDocs extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="page-body">
+          <div className="card">
+            <div className="card-header">Buttons</div>
+            <div className="card-body">
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title">Primary Buttons</div>
+                  <div className="card-text">
+                    <button className="btn-primary">.btn-primary</button>
+                    <button className="btn-secondary">.btn-secondary</button>
+                    <button className="btn-success">.btn-success</button>
+                    <button className="btn-info">.btn-info</button>
+                    <button className="btn-warning">.btn-warning</button>
+                    <button className="btn-danger">.btn-danger</button>
+                    <button className="btn-link">.btn-link</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title">Outline Buttons</div>
+                  <div className="card-text">
+                    <button className="btn-outline-primary">.btn-outline-primary</button>
+                    <button className="btn-outline-secondary">.btn-outline-secondary</button>
+                    <button className="btn-outline-success">.btn-outline-success</button>
+                    <button className="btn-outline-info">.btn-outline-info</button>
+                    <button className="btn-outline-warning">.btn-outline-warning</button>
+                    <button className="btn-outline-danger">.btn-outline-danger</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title">Button Sizes</div>
+                  <div className="card-text">
+                    <button className="btn-primary btn-sm">.btn-primary .btn-sm</button>
+                    <button className="btn-primary">.btn-primary</button>
+                    <button className="btn-primary btn-lg">.btn-primary .btn-lg</button>
+                    <button className="btn-primary btn-full">.btn-primary .btn-full</button>
+                  </div>
+                  <div className="card-text">
+                    <button className="btn-outline-primary btn-sm">.btn-outline-primary .btn-sm</button>
+                    <button className="btn-outline-primary">.btn-outline-primary</button>
+                    <button className="btn-outline-primary btn-lg">.btn-outline-primary .btn-lg</button>
+                    <button className="btn-outline-primary btn-full">.btn-outline-primary .btn-full</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title">Disabled Buttons</div>
+                  <div className="card-text">
+                    <button className="btn-primary btn-disabled">.btn-primary btn-disabled</button>
+                    <button className="btn-secondary btn-disabled">.btn-secondary btn-disabled</button>
+                    <button className="btn-success btn-disabled">.btn-success btn-disabled</button>
+                    <button className="btn-info btn-disabled">.btn-info btn-disabled</button>
+                    <button className="btn-warning btn-disabled">.btn-warning btn-disabled</button>
+                    <button className="btn-danger btn-disabled">.btn-danger btn-disabled</button>
+                    <button className="btn-link btn-disabled">.btn-link btn-disabled</button>
+                  </div>
+                  <div className="card-text">
+                    <button className="btn-outline-primary btn-disabled">.btn-outline-primary btn-disabled</button>
+                    <button className="btn-outline-secondary btn-disabled">.btn-outline-secondary btn-disabled</button>
+                    <button className="btn-outline-success btn-disabled">.btn-outline-success btn-disabled</button>
+                    <button className="btn-outline-info btn-disabled">.btn-outline-info btn-disabled</button>
+                    <button className="btn-outline-warning btn-disabled">.btn-outline-warning btn-disabled</button>
+                    <button className="btn-outline-danger btn-disabled">.btn-outline-danger btn-disabled</button>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title">Icons Buttons</div>
+                  <div className="card-text">
+                    <button className="btn-primary">
+                      <i className="material-icons">add</i>
+                      .btn-primary
+                    </button>
+                    <button className="btn-secondary">
+                      <i className="material-icons">add</i>
+                      .btn-secondary
+                    </button>
+                    <button className="btn-success ">
+                      <i className="material-icons">add</i>
+                      .btn-success
+                    </button>
+                    <button className="btn-info">
+                      <i className="material-icons">add</i>
+                      .btn-info
+                    </button>
+                    <button className="btn-warning">
+                      <i className="material-icons">add</i>
+                      .btn-warning
+                    </button>
+                    <button className="btn-danger">
+                      <i className="material-icons">add</i>
+                      .btn-danger
+                    </button>
+                    <button className="btn-link">
+                      <i className="material-icons">add</i>
+                      .btn-link
+                    </button>
+                  </div>
+                  <div className="card-text">
+                    <button className="btn-outline-primary">
+                      <i className="material-icons">add</i>
+                      .btn-outline-primary
+                    </button>
+                    <button className="btn-outline-secondary">
+                      <i className="material-icons">add</i>
+                      .btn-outline-secondary
+                    </button>
+                    <button className="btn-outline-success">
+                      <i className="material-icons">add</i>
+                      .btn-outline-success
+                    </button>
+                    <button className="btn-outline-info">
+                      <i className="material-icons">add</i>
+                      .btn-outline-info
+                    </button>
+                    <button className="btn-outline-warning">
+                      <i className="material-icons">add</i>
+                      .btn-outline-warning
+                    </button>
+                    <button className="btn-outline-danger">
+                      <i className="material-icons">add</i>
+                      .btn-outline-danger
+                    </button>
+                  </div>
+                  <div className="card-text">
+                    <button className="btn-primary">
+                      .btn-primary
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-secondary">
+                      .btn-secondary
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-success ">
+                      .btn-success
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-info">
+                      .btn-info
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-warning">
+                      .btn-warning
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-danger">
+                      .btn-danger
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-link">
+                      .btn-link
+                      <i className="material-icons">add</i>
+                    </button>
+                  </div>
+                  <div className="card-text">
+                    <button className="btn-outline-primary">
+                      .btn-outline-primary
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-secondary">
+                      .btn-outline-secondary
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-success">
+                      .btn-outline-success
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-info">
+                      .btn-outline-info
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-warning">
+                      .btn-outline-warning
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-danger">
+                      .btn-outline-danger
+                      <i className="material-icons">add</i>
+                    </button>
+                  </div>
+                  <div className="card-text">
+                    <button className="btn-primary">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-secondary">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-success ">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-info">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-warning">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-danger">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-link">
+                      <i className="material-icons">add</i>
+                    </button>
+                  </div>
+                  <div className="card-text">
+                    <button className="btn-outline-primary">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-secondary">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-success">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-info">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-warning">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-danger">
+                      <i className="material-icons">add</i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title">Circular Buttons</div>
+                  <div className="card-text">
+                    <button className="btn-primary btn-circle btn-sm">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-secondary btn-circle">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-success btn-circle btn-lg">
+                      <i className="material-icons">add</i>
+                    </button>
+                  </div>
+                  <div className="card-text">
+                    <button className="btn-outline-primary btn-circle btn-sm">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-secondary btn-circle">
+                      <i className="material-icons">add</i>
+                    </button>
+                    <button className="btn-outline-success btn-circle btn-lg">
+                      <i className="material-icons">add</i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ButtonDocs;
