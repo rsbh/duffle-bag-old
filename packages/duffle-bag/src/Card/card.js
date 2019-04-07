767a2d6bb51ce6b1d2e01c76f4cdabb0b8ec909e
Rishabh Mishra <mishrarishabh865@gmail.com>
2019-04-07 20:27:46 +0530

    add style for card component

--- packages/duffle-bag/src/Card/card.js
+++ packages/duffle-bag/src/Card/card.js
@@ -1,6 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
-const Card = styled.div``;
+const Card = styled.div`
+  background: #fff;
+  color: #666;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.08);
+  display: inline-flex;
+  margin: 4px;
+  padding: 4px;
+  position: relative;
+  box-sizing: border-box;
+  transition: box-shadow 0.1s ease-in-out;
+  border-radius: 4px;
+  height: ${props => props.height};
+  width: ${props => props.width || "max-content"};
+`;
 
 export default Card;
--- packages/duffle-bag/src/Card/card.stories.js
+++ packages/duffle-bag/src/Card/card.stories.js
@@ -2,4 +2,17 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Card from "./card";
 
-storiesOf("Card", module).add("with text", () => <Card>Hello Card</Card>);
+storiesOf("Card", module)
+  .add("with text", () => <Card>Hello Card</Card>)
+  .add("Multiple card", () => (
+    <React.Fragment>
+      <Card>Hello Card</Card>
+      <Card>Hello Card</Card>
+      <Card>Hello Card</Card>
+    </React.Fragment>
+  ))
+  .add("With Custom Height and width", () => (
+    <Card height={"20vh"} width={"500px"}>
+      Card
+    </Card>
+  ));
